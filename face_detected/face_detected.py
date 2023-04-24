0de00b446cfa65e19334e5faed601d5a01a71cb6
Po-Chun,Yeh <111834126+ImitatedSky@users.noreply.github.com>
2023-04-24 10:31:54 +0800

    Update face_detected.py

--- face_detected/face_detected.py
+++ face_detected/face_detected.py
@@ -1,1 +1,30 @@
+import numpy as np
+import cv2
+import os
 
+video_capture = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml") # 載入人臉模型
+
+# faces = face_cascade.detectMultiScale(gray)  # 偵測人臉
+
+if not video_capture.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    ret, frame = video_capture.read()
+    if not ret:
+        print("Cannot receive frame")
+        break
+        
+    frame = cv2.resize(frame,(640,480))              # 縮小尺寸，避免尺寸過大導致效能不好
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # 將鏡頭影像轉換成灰階
+    faces = face_cascade.detectMultiScale(gray)      # 偵測人臉
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)   # 標記人臉
+    cv2.imshow('detected', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
