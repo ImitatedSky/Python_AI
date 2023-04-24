bccaf0fe3da6df9dc22e72da5c88529b468c4ce2
Po-Chun,Yeh <111834126+ImitatedSky@users.noreply.github.com>
2023-04-24 13:01:33 +0800

    Update face_detected.py

--- face_detected/face_detected.py
+++ face_detected/face_detected.py
@@ -10,7 +10,7 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalf
 if not video_capture.isOpened():
     print("Cannot open camera")
     exit()
-while True:
+while (video_capture.isOpened()):
     ret, frame = video_capture.read()
     if not ret:
         print("Cannot receive frame")
